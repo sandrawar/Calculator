ff7ababbfb926e790b38d23f048c33ebda7e3271
Sandra Adamiec <sandra.a3@o2.pl>
2022-10-15 11:49:17 +0200

    operation type read

--- Calculator/sa_count.c
+++ Calculator/sa_count.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 const int max_dl = 41;
 const int max_do = 7;
 
+char wczytajZnak(char *operacja){
+    //printf("%c", *operacja);
+    if(isdigit(*operacja)){
+        printf("%c\n", '>');
+        return '>';
+    }
+    printf("znak: %c\n", *operacja);
+    return *operacja;
+}
+
+int wczytajSystem(char *operacja){
+    int system = 0;
+    while(*operacja != ' '){
+        //printf("%c", operacja);
+        *operacja++;
+    }
+    *operacja++;
+    while(*operacja != '\0' && *operacja != '\n'){
+        //printf("%d\n", system);
+        system *= 10;
+        system += *operacja++ - '0';
+    }
+    printf("system: %d\n", system);
+    return system;
+}
+
 int odczytaj_liczbe(char* liczba, int podstawa){
     int n = 0;
-
+    //printf("podstawa: %d\n", podstawa);
     while (*liczba != '\n' && *liczba != '\0'){
         int cyfra = *liczba++ - '0';
         if (cyfra < 0){
@@ -19,7 +46,8 @@ int odczytaj_liczbe(char* liczba, int podstawa){
     return n;
 }
 
-int wczytajLiczbe(FILE *fptr){
+int wczytajLiczbe(FILE *fptr, int system){
+    printf("sys: %d\n", system);
     char liczba[max_dl];
     if (fgets(liczba, max_dl, fptr) == NULL){
         return -1;
@@ -51,8 +79,18 @@ int wykonajDzialanie(FILE *fptr){
         return -1;
     }
     printf("\nop: %s", operacja);
+    char znak = wczytajZnak(operacja);
+    int podstawa;
+    if(znak != '>'){
+        podstawa = wczytajSystem(operacja);
+    }
+    else{
+        podstawa = 10;
+        //int system = wczytajLiczbe(operacja, 10);
+    }
     int liczba;
-    while((liczba = wczytajLiczbe(fptr)) >= 0) ;
+    printf("%d\n", podstawa);
+    while((liczba = wczytajLiczbe(fptr, podstawa)) >= 0) ;
     
     return 0;
 }
