58f4469d4e0437a901d262b3683e28b61ca474e1
Sandra Adamiec <sandra.a3@o2.pl>
2022-10-15 21:30:02 +0200

    file out creating

--- Calculator/sa_count.c
+++ Calculator/sa_count.c
@@ -6,10 +6,63 @@
 const int max_dl = 41;
 const int max_do = 7;
 
+int zapiszWynik(int wartosc, int podstawa, FILE *fptr2){
+    int wynik[max_dl];
+    for(int i = 0; i < max_dl; i++){
+        wynik[i] = -1;
+    }
+    int liczba = wartosc;
+    int i = max_dl - 1;
+    while(liczba > 0){
+        int cyfra = liczba % podstawa;
+        if(cyfra < 10){
+            wynik[i] = cyfra + '0';
+        }
+        else{
+           wynik[i] = cyfra + 'A' - 11;
+        }
+        //printf("%d ", cyfra);
+        liczba /= podstawa;
+        i --;
+    }
+    printf("\nwynik: ");
+    for(int i = 0; i < max_dl; i++){
+        if(wynik[i] != -1){
+            printf("%c", wynik[i]);
+            fputc(wynik[i], fptr2);
+        }
+    }
+    fputs("\n\n\n", fptr2);
+    printf("\n");
+    return 0;
+    //printf("\nwynik: %s\n", wynik);
+}
+
+int policz(int liczba1, int liczba2, char znak){
+    //printf("liczba1: %d\n", liczba1);
+    if(znak == '+'){
+        //printf("wynik: %d\n", liczba1 + liczba2);
+        return liczba1 + liczba2;
+    }
+    if(znak == '*'){
+        return liczba1 * liczba2;
+    }
+    if(znak == '%'){
+        return liczba1 % liczba2;
+    }
+    if(znak == '^'){
+        return liczba1 ^ liczba2;
+    }
+    if(znak == '/'){
+        return liczba1 / liczba2;
+    }
+    return -1;
+}
+
 char wczytajZnak(char *operacja){
     //printf("%c", *operacja);
     if(isdigit(*operacja)){
-        printf("%c\n", '>');
+        //printf("%c\n", '>');
         return '>';
     }
     printf("znak: %c\n", *operacja);
@@ -28,6 +81,9 @@ int wczytajSystem(char *operacja){
         system *= 10;
         system += *operacja++ - '0';
     }
+    if(system < 2 || system > 16){
+        return -1;
+    }
     printf("system: %d\n", system);
     return system;
 }
@@ -36,7 +92,19 @@ int odczytaj_liczbe(char* liczba, int podstawa){
     int n = 0;
     //printf("podstawa: %d\n", podstawa);
     while (*liczba != '\n' && *liczba != '\0'){
-        int cyfra = *liczba++ - '0';
+        int cyfra;
+        if(isdigit(*liczba)){
+            if(*liczba - '0' >= podstawa){
+                return -2;
+            }
+            cyfra = *liczba++ - '0';
+        }
+        else{
+            if(*liczba - 'A' + 10 >= podstawa){
+                return -3;
+            }
+            cyfra = *liczba++ - 'A' + 10;
+        }
         if (cyfra < 0){
             return -1;
         }
@@ -46,8 +114,8 @@ int odczytaj_liczbe(char* liczba, int podstawa){
     return n;
 }
 
-int wczytajLiczbe(FILE *fptr, int system){
-    printf("sys: %d\n", system);
+int wczytajLiczbe(FILE *fptr, FILE *fptr2, int system){
+    //printf("sys: %d\n", system);
     char liczba[max_dl];
     if (fgets(liczba, max_dl, fptr) == NULL){
         return -1;
@@ -60,17 +128,19 @@ int wczytajLiczbe(FILE *fptr, int system){
     }
     if (liczba[0] != '\n'){
         printf("liczba odczytana: %s", liczba);
-        int wartosc = odczytaj_liczbe(liczba, 10);
+        fputs(liczba, fptr2);
+        fputc('\n', fptr2);
+        int wartosc = odczytaj_liczbe(liczba, system);
         printf("wartosc odczytana: %d\n", wartosc);
         if (wartosc < 0){
             return -5;
         }
-        return 0;
+        return wartosc;
     }
     return -4;
 }
 
-int wykonajDzialanie(FILE *fptr){
+int wykonajDzialanie(FILE *fptr, FILE *fptr2){
     char operacja[max_do];
     if (fgets(operacja, max_do, fptr) == NULL){
         return -1;
@@ -79,28 +149,58 @@ int wykonajDzialanie(FILE *fptr){
         return -1;
     }
     printf("\nop: %s", operacja);
+    fputs(operacja, fptr2);
+    fputc('\n', fptr2);
     char znak = wczytajZnak(operacja);
-    int podstawa;
+    int podstawa = 0;
+    int wynik;
     if(znak != '>'){
         podstawa = wczytajSystem(operacja);
+        wynik = wczytajLiczbe(fptr, fptr2, podstawa);
+        int liczba;
+        while((liczba = wczytajLiczbe(fptr, fptr2, podstawa)) >= 0){
+            //liczba = wczytajLiczbe(fptr, podstawa);
+            //fputs(liczba, fptr2);
+            wynik = policz(wynik, liczba, znak);
+            //printf("%d\n", wynik);
+        }
+        printf("wynik: %d\n", wynik);
+        zapiszWynik(wynik, podstawa, fptr2);
     }
     else{
-        podstawa = 10;
+        int nowaPodstawa = 0;
+        int i = 0;
+        for(int j = 0; operacja[j] != ' '; j++){
+            podstawa *= 10;
+            podstawa += operacja[j] - '0';
+            i++;
+        }
+        i++;
+        //printf("%d\n", i);
+        for(i; operacja[i] != '\0' && operacja[i] != '\n'; i++){
+            nowaPodstawa *= 10;
+            nowaPodstawa += operacja[i] - '0';
+        }
+        printf("nowa podstawa: %d\n", nowaPodstawa);
+        int liczba;
+        while((liczba = wczytajLiczbe(fptr, fptr2, podstawa)) >= 0){
+            zapiszWynik(liczba, nowaPodstawa, fptr2);
+        }
         //int system = wczytajLiczbe(operacja, 10);
     }
-    int liczba;
-    printf("%d\n", podstawa);
-    while((liczba = wczytajLiczbe(fptr, podstawa)) >= 0) ;
-    
+    //printf("%d\n", podstawa);
     return 0;
 }
 
 int main(){
     FILE *fptr;
     fptr = fopen("sa_01_in.txt", "r");
+    FILE *fptr2;
+    fptr2 = fopen("sa_01_out.txt", "w");
     while (!feof(fptr)){
-        wykonajDzialanie(fptr);
+        wykonajDzialanie(fptr, fptr2);
     }
-    fclose(fptr); 
+    fclose(fptr);
+    fclose(fptr2); 
     return 0;
 }
