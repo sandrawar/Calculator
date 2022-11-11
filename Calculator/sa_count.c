a5d39dfebd8868df507d5e137129ace4c2246984
Sandra Adamiec <sandra.a3@o2.pl>
2022-11-11 23:16:17 +0100

    refactored to use char[] as calc arguments (numbers can have 40 chars which is too big for int or long)

--- Calculator/sa_count.c
+++ Calculator/sa_count.c
@@ -3,17 +3,26 @@
 #include <string.h>
 #include <ctype.h>
 
-const int max_dl_liczby = 40;
-const int max_dl_linii = max_dl_liczby + 1;
-const int max_do = 7;
+#define max_dl_liczby 40
+#define max_dl_linii (max_dl_liczby + 1)
+#define max_do 7
+
+enum Operacja {dodawanie = 1, mnozenie = 2, potegowanie = 3, dzielenie = 4, modulo = 5, zmiana_podstawy = 6, nieznana = 0};
 
 int wykonajDzialanie(FILE *fptr, FILE *fptr2);
-int wczytajLiczbe(FILE *fptr, FILE *fptr2, int system);
-int odczytaj_liczbe(char* liczba, int podstawa);
-int wczytajSystem(char *operacja);
-char wczytajZnak(char *operacja);
-int policz(int liczba1, int liczba2, char znak);
-int zapiszWynik(int wartosc, int podstawa, FILE *fptr2);
+enum Operacja wczytajOperacje(char *operacja);
+int wczytajPodstawe(char *operacja);
+int wczytajLiczbe(FILE *fptr, FILE *fptr2, char* liczba, int podstawa);
+int zamienNaLiczby(char* znaki, char* liczba, int podstawa);
+int zamianaPodstawy(char* liczba, char* wynik, int podstawa, int nowapodstawa);
+int policz(char* liczba1, char* liczba2, char* wynik, int podstawa, enum Operacja operacja);
+int wykonajDodawanie(char* liczba1, char* liczba2, char* wynik, int podstawa);
+int wykonajMnozenie(char* liczba1, char* liczba2, char* wynik, int podstawa);
+int wykonajPotegowanie(char* liczba1, char* liczba2, char* wynik, int podstawa);
+int wykonajDzielenie(char* liczba1, char* liczba2, char* wynik, int podstawa);
+int wykonajModulo(char* liczba1, char* liczba2, char* wynik, int podstawa);
+int zapiszWynik(char* wynik, int podstawa, FILE *fptr2);
+char zamienNaZnak(char liczba);
 
 int main(int argc, char *argv[]){
     char *fin = "sa_01_in.txt";
@@ -37,188 +46,328 @@ int main(int argc, char *argv[]){
 }
 
 int wykonajDzialanie(FILE *fptr, FILE *fptr2){
-    char operacja[max_do];
-    if (fgets(operacja, max_do, fptr) == NULL){
+    char definicja_operacji[max_do];
+    if (fgets(definicja_operacji, max_do, fptr) == NULL){
         return -1;
     };
-    if (operacja[0] == '\n'){
+    if (definicja_operacji[0] == '\n'){
         return -1;
     }
-    printf("\nop: %s", operacja);
-    fputs(operacja, fptr2);
+    printf("\nop: %s", definicja_operacji);
+    fputs(definicja_operacji, fptr2);
     fputc('\n', fptr2);
-    char znak = wczytajZnak(operacja);
-    int podstawa = 0;
-    int wynik;
-    if(znak != '>'){
-        podstawa = wczytajSystem(operacja);
-        wynik = wczytajLiczbe(fptr, fptr2, podstawa);
-        int liczba;
-        while((liczba = wczytajLiczbe(fptr, fptr2, podstawa)) >= 0){
-            //liczba = wczytajLiczbe(fptr, podstawa);
-            //fputs(liczba, fptr2);
-            wynik = policz(wynik, liczba, znak);
-            //printf("%d\n", wynik);
-        }
-        printf("wynik: %d\n", wynik);
-        zapiszWynik(wynik, podstawa, fptr2);
+    enum Operacja operacja = wczytajOperacje(definicja_operacji);
+    printf("operacja %d\n", operacja);
+    if (operacja == nieznana){
+        return -2;
     }
-    else{
-        int nowaPodstawa = 0;
-        int i = 0;
-        for(int j = 0; operacja[j] != ' '; j++){
-            podstawa *= 10;
-            podstawa += operacja[j] - '0';
-            i++;
+    int podstawa = wczytajPodstawe(definicja_operacji);
+    if (podstawa <= 0){
+        return -3;
+    }
+    printf("podstawa %d\n", podstawa);
+    char *liczba1 = malloc(sizeof(char) * max_dl_liczby);
+    char *liczba2 = malloc(sizeof(char) * max_dl_liczby);
+    char *wynik = malloc(sizeof(char) * max_dl_liczby);
+
+    if(operacja == zmiana_podstawy){
+        char* npStart = definicja_operacji;
+        while (isdigit(*npStart)){
+            npStart++;
         }
-        i++;
-        //printf("%d\n", i);
-        for(i; operacja[i] != '\0' && operacja[i] != '\n'; i++){
-            nowaPodstawa *= 10;
-            nowaPodstawa += operacja[i] - '0';
-        }
+        int nowaPodstawa = wczytajPodstawe(npStart);
         printf("nowa podstawa: %d\n", nowaPodstawa);
         int liczba;
-        while((liczba = wczytajLiczbe(fptr, fptr2, podstawa)) >= 0){
-            zapiszWynik(liczba, nowaPodstawa, fptr2);
+        while((wczytajLiczbe(fptr, fptr2, liczba1, podstawa)) >= 0){
+            printf("liczba1: %s\n", liczba1);
+            if (zamianaPodstawy(liczba1, wynik, podstawa, nowaPodstawa) >= 0){
+                printf("wynik: %s\n", wynik);
+                zapiszWynik(wynik, nowaPodstawa, fptr2);
+            }
         }
-        //int system = wczytajLiczbe(operacja, 10);
     }
-    //printf("%d\n", podstawa);
+    else{
+        if (wczytajLiczbe(fptr, fptr2, wynik, podstawa) == 0){
+            printf("liczba1 (wynik): %s\n", wynik);
+            int liczba;
+            while((wczytajLiczbe(fptr, fptr2, liczba2, podstawa)) == 0){
+                char* tmp = wynik;
+                wynik = liczba1;
+                liczba1 = tmp;
+                printf("liczba1: %s\n", liczba1);
+                printf("liczba2: %s\n", liczba2);
+                policz(liczba1, liczba2, wynik, podstawa, operacja);
+                printf("wynik: %s\n", wynik);
+            }
+            printf("wynik: %s\n", wynik);
+            zapiszWynik(wynik, podstawa, fptr2);
+        }
+    }
+    free(liczba1);
+    free(liczba2);
+    free(wynik);
+
     return 0;
 }
 
-int wczytajLiczbe(FILE *fptr, FILE *fptr2, int system){
-    //printf("sys: %d\n", system);
-    char liczba[max_dl_linii];
-    if (fgets(liczba, max_dl_linii, fptr) == NULL){
+enum Operacja wczytajOperacje(char *operacja){
+    if(isdigit(*operacja)){
+        return zmiana_podstawy;
+    }
+    if(*operacja == '+'){
+        return dodawanie;
+    }
+    if(*operacja == '*'){
+        return mnozenie;
+    }
+    if(*operacja == '%'){
+        return modulo;
+    }
+    if(*operacja == '^'){
+        return potegowanie;
+    }
+    if(*operacja == '/'){
+        return dzielenie;
+    }
+    return nieznana;
+}
+
+int wczytajPodstawe(char *operacja){
+    int podstawa = 0;
+    while(*operacja != '\0' && *operacja != '\n' && !isdigit(*operacja)){
+        operacja++;
+    }
+    if (!isdigit(*operacja)){
         return -1;
     }
-    if (liczba[0] != '\n'){
+    while(*operacja != '\0' && *operacja != '\n'&& isdigit(*operacja)){
+        podstawa *= 10;
+        podstawa += *operacja++ - '0';
+    }
+    if(podstawa < 2 || podstawa > 16){
         return -2;
     }
-    if (fgets(liczba, max_dl_linii, fptr) == NULL){
+    return podstawa;
+}
+
+int wczytajLiczbe(FILE *fptr, FILE *fptr2, char* liczba, int podstawa){
+    char bufor[max_dl_linii];
+    if (fgets(bufor, max_dl_linii, fptr) == NULL){
+        return -1;
+    }
+    if (bufor[0] != '\n'){
+        return -2;
+    }
+    if (fgets(bufor, max_dl_linii, fptr) == NULL){
         return -3;
     }
-    if (liczba[0] != '\n'){
-        printf("liczba odczytana: %s", liczba);
-        fputs(liczba, fptr2);
-        fputc('\n', fptr2);
-        int wartosc = odczytaj_liczbe(liczba, system);
-        printf("wartosc odczytana: %d\n", wartosc);
-        if (wartosc < 0){
-            return -5;
+    if (bufor[0] != '\n'){
+        fputs(bufor, fptr2);
+        fputs("\n", fptr2);
+        size_t len = strlen(bufor);
+        if (len > 0 && bufor[--len] == '\n') {
+            bufor[len] = '\0';
         }
-        return wartosc;
+        printf("odczyt: %s\n", bufor);
+        zamienNaLiczby(bufor, liczba, podstawa);
+        return 0;
     }
     return -4;
  }
 
-int odczytaj_liczbe(char* liczba, int podstawa){
-    int n = 0;
-    //printf("podstawa: %d\n", podstawa);
-    while (*liczba != '\n' && *liczba != '\0'){
-        int cyfra;
-        if(isdigit(*liczba)){
-            if(*liczba - '0' >= podstawa){
-                return -2;
-            }
-            cyfra = *liczba++ - '0';
-        }
-        else{
-            if(*liczba - 'A' + 10 >= podstawa){
-                return -3;
-            }
-            cyfra = *liczba++ - 'A' + 10;
-        }
-        if (cyfra < 0){
-            return -1;
-        }
-        n = n * podstawa + cyfra;
-    }
-
-    return n;
-}
-
-int wczytajSystem(char *operacja){
-    int system = 0;
-    while(*operacja != ' '){
-        //printf("%c", operacja);
-        *operacja++;
-    }
-    *operacja++;
-    while(*operacja != '\0' && *operacja != '\n'){
-        //printf("%d\n", system);
-        system *= 10;
-        system += *operacja++ - '0';
-    }
-    if(system < 2 || system > 16){
+int zamienNaLiczby(char* znaki, char* liczba, int podstawa){
+    int len = strlen(znaki);
+    if (len <= 0){
         return -1;
     }
-    printf("system: %d\n", system);
-    return system;
-}
 
-char wczytajZnak(char *operacja){
-    //printf("%c", *operacja);
-    if(isdigit(*operacja)){
-        //printf("%c\n", '>');
-        return '>';
-    }
-    printf("znak: %c\n", *operacja);
-    return *operacja;
-}
-
-int policz(int liczba1, int liczba2, char znak){
-    //printf("liczba1: %d\n", liczba1);
-    if(znak == '+'){
-        //printf("wynik: %d\n", liczba1 + liczba2);
-        return liczba1 + liczba2;
-    }
-    if(znak == '*'){
-        return liczba1 * liczba2;
-    }
-    if(znak == '%'){
-        return liczba1 % liczba2;
-    }
-    if(znak == '^'){
-        return liczba1 ^ liczba2;
-    }
-    if(znak == '/'){
-        return liczba1 / liczba2;
-    }
-    return -1;
-}
-
-int zapiszWynik(int wartosc, int podstawa, FILE *fptr2){
-    int wynik[max_dl_linii];
-    for(int i = 0; i < max_dl_linii; i++){
-        wynik[i] = -1;
-    }
-    int liczba = wartosc;
-    int i = max_dl_linii - 1;
-    while(liczba > 0){
-        int cyfra = liczba % podstawa;
-        if(cyfra < 10){
-            wynik[i] = cyfra + '0';
+    int i = max_dl_liczby;
+    //char* znak = znaki + sizeof(char) * len;
+    while (--len >= 0)
+    {
+        int cyfra;
+        if(isdigit(znaki[len])){
+            cyfra = znaki[len] - '0';
+            if(cyfra >= podstawa){
+                return -2;
+            }
         }
         else{
-           wynik[i] = cyfra + 'A' - 11;
+            cyfra = znaki[len] - 'A' + 10;
+            if(cyfra >= podstawa){
+                return -3;
+            }
         }
-        //printf("%d ", cyfra);
-        liczba /= podstawa;
-        i --;
-    }
-    printf("\nwynik: ");
-    for(int i = 0; i < max_dl_linii; i++){
-        if(wynik[i] != -1){
-            printf("%c", wynik[i]);
-            fputc(wynik[i], fptr2);
+        if (cyfra < 0){
+            return -4;
         }
+        liczba[--i] = cyfra;
     }
-    fputs("\n\n\n", fptr2);
+    while (--i >= 0){
+        liczba[i] = 0;
+    }
+    printf("liczby: ");
+    for(int i = 0; i < max_dl_liczby; i++){
+        printf("%d,", liczba[i]);
+    }
     printf("\n");
-    return 0;
-    //printf("\nwynik: %s\n", wynik);
 }
+
+int zamianaPodstawy(char* liczba, char* wynik, int podstawa, int nowaPodstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba[i];
+    }
+    return 0;
+}
+
+int policz(char* liczba1, char* liczba2, char* wynik, int podstawa, enum Operacja operacja){
+    switch (operacja)
+    {
+        case dodawanie:
+            return wykonajDodawanie(liczba1, liczba2, wynik, podstawa);
+
+        case mnozenie:
+            return wykonajMnozenie(liczba1, liczba2, wynik, podstawa);
+
+        case modulo:
+            return wykonajModulo(liczba1, liczba2, wynik, podstawa);
+
+        case potegowanie:
+            return wykonajPotegowanie(liczba1, liczba2, wynik, podstawa);
+
+        case dzielenie:
+            return wykonajDzielenie(liczba1, liczba2, wynik, podstawa);
+
+        default:
+            return -1;
+    }
+}
+
+int wykonajDodawanie(char* liczba1, char* liczba2, char* wynik, int podstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba2[i];
+    }
+}
+
+int wykonajMnozenie(char* liczba1, char* liczba2, char* wynik, int podstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba2[i];
+    }
+}
+
+int wykonajPotegowanie(char* liczba1, char* liczba2, char* wynik, int podstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba2[i];
+    }
+}
+
+int wykonajDzielenie(char* liczba1, char* liczba2, char* wynik, int podstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba2[i];
+    }
+}
+
+int wykonajModulo(char* liczba1, char* liczba2, char* wynik, int podstawa){
+    for(int i=0; i < max_dl_liczby; i++){
+        wynik[i] = liczba2[i];
+    }
+}
+
+int zapiszWynik(char* wynik, int podstawa, FILE *fptr2){
+    printf("zapiszWynik: ");
+    int i = 0;
+    while (i < max_dl_liczby && wynik[i] == 0){
+        i++;
+    }
+    while (i < max_dl_liczby){
+        char znak = zamienNaZnak(wynik[i++]); 
+        printf("%c", znak);
+        fputc(znak, fptr2);
+    }
+    printf("\n");
+    fputs("\n\n\n", fptr2);
+    return 0;
+}
+
+char zamienNaZnak(char liczba){
+    return (liczba < 10) ? liczba + '0' : liczba - 10 + 'A';
+}
+
+// int odczytaj_liczbe(char* liczba, int podstawa){
+//     int n = 0;
+//     while (*liczba != '\n' && *liczba != '\0'){
+//         int cyfra;
+//         if(isdigit(*liczba)){
+//             if(*liczba - '0' >= podstawa){
+//                 return -2;
+//             }
+//             cyfra = *liczba++ - '0';
+//         }
+//         else{
+//             if(*liczba - 'A' + 10 >= podstawa){
+//                 return -3;
+//             }
+//             cyfra = *liczba++ - 'A' + 10;
+//         }
+//         if (cyfra < 0){
+//             return -1;
+//         }
+//         n = n * podstawa + cyfra;
+//     }
+
+//     return n;
+// }
+
+// int policz(int liczba1, int liczba2, enum Operacja operacja){
+//     switch (operacja){
+//         case dodawanie:
+//             return liczba1 + liczba2;
+        
+//         case mnozenie:
+//             return liczba1 * liczba2;
+
+//         case modulo:
+//             return liczba1 % liczba2;
+
+//         case potegowanie:
+//             return liczba1 ^ liczba2;
+
+//         case dzielenie:
+//             return liczba1 / liczba2;
+
+//         default:
+//         return -1;
+//     }
+// }
+
+// int zapiszWynik(int wartosc, int podstawa, FILE *fptr2){
+//     int wynik[max_dl_linii];
+//     for(int i = 0; i < max_dl_linii; i++){
+//         wynik[i] = -1;
+//     }
+//     int liczba = wartosc;
+//     int i = max_dl_linii - 1;
+//     while(liczba > 0){
+//         int cyfra = liczba % podstawa;
+//         if(cyfra < 10){
+//             wynik[i] = cyfra + '0';
+//         }
+//         else{
+//            wynik[i] = cyfra + 'A' - 11;
+//         }
+//         liczba /= podstawa;
+//         i --;
+//     }
+//     printf("\nwynik: ");
+//     for(int i = 0; i < max_dl_linii; i++){
+//         if(wynik[i] != -1){
+//             printf("%c", wynik[i]);
+//             fputc(wynik[i], fptr2);
+//         }
+//     }
+//     fputs("\n\n\n", fptr2);
+//     printf("\n");
+//     return 0;
+//     //printf("\nwynik: %s\n", wynik);
+// }
 
